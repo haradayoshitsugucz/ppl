de8d114517c8ad3b9bf78989c6738063b7e9e81e
harada-yoshitsugu <harada-yoshitsugu@cyber-z.co.jp>
2022-05-26 19:06:11 +0900

    fix

--- api/env/env.go
+++ api/env/env.go
@@ -17,32 +17,5 @@ func NewConfig(env string) config.Config {
 	default:
 		log.Panic(fmt.Sprintf("env引数が不正なため、起動に失敗しました en%+v\n", env))
 	}
-}
-
-// NewConfig2 config初期化
-func NewConfig2(env string) config.Config {
-	var conf config.Config
-	switch env {
-	case config.Local:
-		conf = NewLocal()
-	case config.Test:
-		conf = NewTest()
-	default:
-		log.Panic(fmt.Sprintf("env引数が不正なため、起動に失敗しました en%+v\n", env))
-	}
-	return conf
-}
-
-// NewConfig3 config初期化
-func NewConfig3(env string) config.Config {
-	var conf config.Config
-	switch env {
-	case config.Local:
-		conf = NewLocal()
-	case config.Test:
-		conf = NewTest()
-	default:
-		log.Panic(fmt.Sprintf("env引数が不正なため、起動に失敗しました en%+v\n", env))
-	}
-	return conf
+	return NewLocal()
 }
